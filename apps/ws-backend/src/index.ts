e2e6ce1167f8cab36b8f4271bc9c383bb8cf5c95
Saurabh Pandey <ampsaurabh88877@gmail.com>
2026-01-06 23:36:36 +0530

    initilize project done

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,8 @@
+import {WebSocketServer} from 'ws';
+const wss= new WebSocketServer({port:8080});
+wss.on('connection', ws=>{
+    ws.on('message', message=>{
+        console.log(`Received message: ${message}`);
+    });
+    ws.send('Hello! Message from server...');
+});

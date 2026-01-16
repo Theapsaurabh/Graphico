fcb2e1aa6c03b197383322c14877dad4b945203a
Saurabh Pandey <ampsaurabh88877@gmail.com>
2026-01-16 11:26:32 +0530

    write signup, sign-in , create-room endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,28 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { middleware } from './middleware';
+import { JWT_SECRET } from './config';
 const app= express();
 app.listen(3001);
+app.post("/signup", (req,res)=>{
+    // db call here 
+    res.json({
+        userId: "123"
+    })
+
+
+})
+app.post("/signin", (req,res)=>{
+    const userId=1;
+    jwt.sign({
+
+        userId
+    }, JWT_SECRET)
+    
+})
+
+app.post("/room",middleware, (req,res)=>{
+    res.json({
+        roomId:123
+    })
+})

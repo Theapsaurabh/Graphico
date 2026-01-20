6da30a68d3351d1e002d6d6c610d350e005c8f42
Saurabh Pandey <ampsaurabh88877@gmail.com>
2026-01-20 22:14:00 +0530

    Added Signup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,16 +3,44 @@ import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { CreateUserSchema } from "@repo/common/types";
+import { PrismaClient } from "@repo/db/client";
+const prisma= new  PrismaClient({} as any)
 const app = express();
 app.listen(3001);
 app.post("/signup", (req, res) => {
-    const data= CreateUserSchema.safeParse(req.body)
-    if(!data.success){
+    const Parsedata= CreateUserSchema.safeParse(req.body)
+    if(! Parsedata.success){
         return res.json({
             message: "Incorrect inputs"
         })
     }
-  // db call here
+  // db call here\
+  try {
+    prisma.user.create({
+  data:{
+  email:  Parsedata.data?.username,
+  password:Parsedata.data.password,
+  name:Parsedata.data.name
+
+
+  }
+
+
+
+ })
+ res.json({
+  userId:"123"
+ })
+
+    
+  } catch (error) {
+    res.status(411).json({
+      message:"User Already exist with this user name"
+    })
+    
+  }
+
+ 
 
   res.json({
     userId: "123",
